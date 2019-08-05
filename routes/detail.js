a96a9642aef124f69658dbaaeec5b1b6946cc7b6
Krico16 <bash_stone_u@hotmail.com>
2019-08-04 19:42:29 -0500

    Añadido pagina de detalle

--- routes/detail.js
+++ routes/detail.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('dashboard', { title: 'Carma Store' });
+    res.render('detail', { title: 'Carma Store' });
 });
 
 module.exports = router;
