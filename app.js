e406b88902b83693109546dfceaf8c5579271f9d
Krico16 <bash_stone_u@hotmail.com>
2019-08-04 20:17:25 -0500

    fixed?

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dashboardRouter = require('./routes/dashboard');
+<<<<<<< HEAD
 var detailRouter = require('./routes/detail');
+=======
+
+>>>>>>> parent of a96a964... Añadido pagina de detalle
 var app = express();
 
 // view engine setup
@@ -22,16 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dashboard', dashboardRouter);
-app.use('/detail', detailRouter);
+app.use('/dashboard',dashboardRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
