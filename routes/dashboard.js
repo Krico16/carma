5a3afa0459b590253adb82146ee1e8919bea399a
Krico16 <bash_stone_u@hotmail.com>
2019-09-21 18:42:33 -0500

    Cosas mejoradas :D

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,12 +6,17 @@ var fb = require("firebase-admin");
 
 router.get('/', async function (req, res, next) {
   var Lista = [];
+  var ListaImg = [];
 
   try {
     var AllGames = await fb.firestore().collection('/Games/yU6CpTHmpn3MiaB14bQd/lista').get();
     AllGames.forEach(doc => {
       //console.log(doc.data());
-      
+
+      ListaImg.push({
+        url: doc.data().photo
+      });
+
       Lista.push({
         id: doc.id,
         data: {
@@ -20,18 +25,19 @@ router.get('/', async function (req, res, next) {
           release: doc.data().date,
           photo: doc.data().photo,
           price: doc.data().price,
-          language: "Faltan la api",
-          company: "Faltan la api"
+          language: "Falta la api",
+          company: "Falta la api"
         }
       });
     });
 
-    res.render("dashboard",{
+    res.render("dashboard", {
       title: "Carma Store",
-      games: Lista
+      games: Lista,
+      Images: ListaImg
     });
   } catch (error) {
-    res.status(200).json({Message: "Error en la pagina" + error});
+    res.status(200).json({ Message: "Error en la pagina" + error });
   }
 });
 
--- routes/detail.js
+++ routes/detail.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var fb = require("firebase-admin");
 
 /* GET home page. */
-router.get('/:id', function (req, res, next) {
-    res.render('detail', { title: 'Carma Store | Juego: ' });
+router.get('/:id', async function (req, res, next) {
+    var Lista = [];
+
+    try {
+        await fb.firestore().collection('/Games/yU6CpTHmpn3MiaB14bQd/lista/')
+            .doc(req.params.id)
+            .get()
+            .then(doc => {
+                res.render('detail', {
+                    title: 'Carma Store | Juego: ', data: {
+                        name: doc.data().name,
+                        info: doc.data().info,
+                        release: doc.data().date,
+                        photo: doc.data().photo,
+                        price: doc.data().price,
+                        language: "Falta la api",
+                        company: "Falta la api"
+                    }
+                });
+            });
+    } catch (error) {
+        res.status(200).json({ Message: "Error en la pagina" + error });
+    }
+
+
 });
 
 module.exports = router;
+
+
