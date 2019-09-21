55eb17140c6912b544edda9ede62344c6b0986f6
Krico16 <bash_stone_u@hotmail.com>
2019-09-21 13:10:50 -0500

    add images

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,12 +10,15 @@ router.get('/', async function (req, res, next) {
   try {
     var AllGames = await fb.firestore().collection('/Games/yU6CpTHmpn3MiaB14bQd/lista').get();
     AllGames.forEach(doc => {
+      //console.log(doc.data());
+      
       Lista.push({
         id: doc.id,
         data: {
           name: doc.data().name,
           info: doc.data().info,
           release: doc.data().date,
+          photo: doc.data().photo,
           price: doc.data().price,
           language: "Faltan la api",
           company: "Faltan la api"
